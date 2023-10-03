763297d132bc8a59760d809e3ecb2adb804c28e1
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-10-03 18:03:53 +0200

    Terminados ejercicios 8 y 10

--- src/ParteAlejandroGarcia05/ej10.java
+++ src/ParteAlejandroGarcia05/ej10.java
@@ -1,4 +1,54 @@
 package ParteAlejandroGarcia05;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ej10 {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+        int[] numerosAleatorios = new int[20];
+        ArrayList<Integer> numerosPares = new ArrayList<>();
+        ArrayList<Integer> numerosImpares = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            numerosAleatorios[i] = rand.nextInt(101);
+        }
+
+        /* Pasar números pares del índice 0 en adelante
+           números impares en celdas restantes */
+
+        for (int i = 0; i < 20; i++) {
+            int numero = numerosAleatorios[i];
+            if (numero % 2 == 0) {
+                numerosPares.add(numero);
+            }
+            else {
+                numerosImpares.add(numero);
+            }
+        }
+
+        System.out.println(numerosPares);
+        System.out.println(numerosImpares);
+
+
+        /* Copio los nº pares del array numerosPares al principio del array
+        de numerosAleatorios y voy sumando 1 a la 'i'  */
+        int i = 0;
+        for (Integer numero : numerosPares) {
+            numerosAleatorios[i++] = numero;
+        }
+
+        // Copio de la misma forma los nº impares al final del array 'numerosAleatorios'
+        for (Integer numero : numerosImpares) {
+            numerosAleatorios[i++] = numero;
+        }
+
+        for (int numero : numerosAleatorios) {
+            System.out.print(numero + " ");
+        }
+
+    }
 }
--- src/ParteAlejandroGarcia05/ej8.java
+++ src/ParteAlejandroGarcia05/ej8.java
@@ -1,4 +1,32 @@
 package ParteAlejandroGarcia05;
 
+import java.util.Scanner;
+
 public class ej8 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] mediaTemperaturas = new int[12];
+        String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
+
+        for (int i = 0; i < 12; i++) {
+            System.out.println("Introduce la temperatura media de " + meses[i] + ": ");
+            int temperatura = scanner.nextInt();
+            mediaTemperaturas[i] = temperatura;
+        }
+
+
+        /* En este bucle for vamos obteniendo las líneas con el caracter '*'
+        de la longitud de cada temperatura media */
+        for (int i = 0; i < 12; i++) {
+            System.out.print(meses[i] + ": ");
+            for (int j = 0; j < mediaTemperaturas[i]; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
